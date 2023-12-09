b17a4eff7765d05dfa66f1e445495186623636da
dprmd <adipr671@gmail.com>
2023-12-09 11:43:53 +0700

    add contact section

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,12 +14,28 @@ hamburger.addEventListener("click", () => {
 // Mabar
 
 const mabar = document.querySelector(".mabar")
+const messageContainer = document.querySelector(".message")
+const messageBg = document.querySelector(".message-bg")
+const messageOk = document.querySelector(".message-ok")
+
+const renderMessage = (message) => {
+  messageContainer.innerHTML = message
+  messageBg.classList.remove("hidden")
+  messageBg.classList.add("flex")
+}
+
+messageOk.addEventListener("click", () => {
+  messageBg.classList.remove("flex")
+  messageBg.classList.add("hidden")
+})
 
 mabar.addEventListener("click", () => {
-  alert(`Hayuk Mabar
-
-USERNAME : Arumijen
-ID GW : 490245606`)
+  const mabarML = `
+  <h3 class="text-lg font-bold">Hayyuk Mabar ML</h3>
+  <p class="text-md font-medium">USERNAME : Arrumijen</p>
+  <p class="text-md font-medium">ID GW    : 490245606</p>
+`
+  renderMessage(mabarML)
 })
 
 // Navbar Sticky
@@ -40,8 +56,25 @@ window.onscroll = () => {
 const modeToggle = document.querySelector(".mode-toggle")
 const modeToggleCircle = document.querySelector(".mode-toggle-circle")
 
+const switchTheme = (mode) => {
+  if (mode === "light") {
+    modeToggleCircle.classList.remove("right-1")
+    modeToggleCircle.classList.add("left-1")
+    localStorage.setItem("theme", "light")
+    html.classList.remove("dark")
+  }
+  if (mode === "dark") {
+    modeToggleCircle.classList.remove("left-1")
+    modeToggleCircle.classList.add("right-1")
+    localStorage.setItem("theme", "dark")
+    html.classList.add("dark")
+  }
+}
+
+if (localStorage.getItem("theme") === "light") switchTheme("light")
+else switchTheme("dark")
+
 modeToggle.addEventListener("click", () => {
-  html.classList.toggle("dark")
-  modeToggleCircle.classList.toggle("translate-x-0")
-  modeToggleCircle.classList.toggle("translate-x-5")
+  if (localStorage.getItem("theme") === "light") switchTheme("dark")
+  else switchTheme("light")
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,27 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#0284c7",
+        primary: "#0369a1",
         primaryMuda: "#0ea5e9",
 
-        secondary: "#ea580c",
+        secondary: "#c2410c",
         secondaryMuda: "#f97316",
 
-        tiga: "#16a34a",
-        tigaMuda: "#22c55e",
+        tiga: "#0f766e",
+        tigaMuda: "#14b8a6",
+
+        empat: "#a21caf",
+        empatMuda: "#d946ef",
 
         light: "#e2e8f0",
         dark: "#0f172a",
 
         darkBg: "#0f172a",
         darkBgMuda: "#1e293b"
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        josefin: ["Josefin Sans", "sans-serif"]
       }
     },
   },
